c822a230cc4608bd8ec98ab8b97b85b2f930083a
Tommy-tung <56158974+Tommy-tung@users.noreply.github.com>
2025-08-03 19:14:45 +0800

    Add files via upload

--- api_test.py
+++ api_test.py
@@ -5,10 +5,9 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def echo():
     # print(request)
-    # data = request.get_json()
-    # print("Received data:", request.data)
-    # return jsonify({"received": request.data})
-    return jsonify({"message": "Hello from updated API!"})
+    data = request.get_json()
+    print("Received data:", data)
+    return jsonify({"received": data})
 
 if __name__ == '__main__':
     app.run()
