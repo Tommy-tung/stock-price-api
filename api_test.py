971edaa05ff1b4154bebeeeb02be98240ce9fb34
Tommy-tung <56158974+Tommy-tung@users.noreply.github.com>
2025-08-03 20:18:10 +0800

    Add files via upload

--- api_test.py
+++ api_test.py
@@ -1,13 +1,16 @@
 from flask import Flask, request, jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/read_excel', methods=['POST'])
 def echo():
     # print(request)
     data = request.get_json()
+    df = pd.read('s&p500_data.xlsx')
+    stock_list = df['代碼'].values[:5]
     print("Received data:", data)
-    return jsonify({"received": data})
+    return jsonify({"received": data, "stock" : stock_list})
 
 if __name__ == '__main__':
     app.run()
